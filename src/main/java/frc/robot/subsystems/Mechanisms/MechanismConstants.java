9e2a0924bbdfab7fd48e00656fbc37ad50b6abe2
Spaceman 11 <135277197+Spaceman113138@users.noreply.github.com>
2025-02-12 09:37:34 -0600

    Update MechanismConstants.java

--- src/main/java/frc/robot/subsystems/Mechanisms/MechanismConstants.java
+++ src/main/java/frc/robot/subsystems/Mechanisms/MechanismConstants.java
@@ -13,10 +13,22 @@ public class MechanismConstants {
   public static final int beltId = 0;
 
 
-  // Elevator Constants
-  public static final double levelOne = 0;
-  public static final double levelTwo = 0;
-  public static final double levelThree = 0;
-  // public final double levelFour = 0;
+  public class ElevatorConstants {
+    public static final double elevatorGearing = 25.0;
+    public static final double elevatorDrumDiam = 0.044704;
+    public static final double elevatorDrumRad = elevatorDrumDiam / 2;
+    public static final double conversion_MS_RPM = 60.0 * Math.PI * elevatorDrumDiam * elevatorGearing);
+    public static final double conversion_RPM_MS = 1.0 / conversion_MS_RPM;
+    public static final double conversion_M_Rot = Math.PI * elevatorDrumDiam * elevatorGearing;
+    public static final double conversion_Rot_M = 1.0 / conversion_M_Rot;
+    
+    
+    // Elevator Constants
+    public static final double levelOne = 0;
+    public static final double levelTwo = 0;
+    public static final double levelThree = 0;
+    // public final double levelFour = 0;
+
+  }
 
 }
