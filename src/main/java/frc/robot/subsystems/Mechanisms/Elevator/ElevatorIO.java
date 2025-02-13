c0a5db923237edb13a35c6f6a098b696a092f9b4
Spaceman 11 <135277197+Spaceman113138@users.noreply.github.com>
2025-02-13 09:27:53 -0600

    Update ElevatorIO.java

--- src/main/java/frc/robot/subsystems/Mechanisms/Elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/Mechanisms/Elevator/ElevatorIO.java
@@ -14,6 +14,7 @@ public interface ElevatorIO {
     public double dutyCycle = 0.0;
     public double appliedVolts = 0.0;
     public double appliedCurrent = 0.0;
+    public double setPoint = 0.0;
   }
 
   default void updateInputs(ElevatorIOInputs inputs) {}
