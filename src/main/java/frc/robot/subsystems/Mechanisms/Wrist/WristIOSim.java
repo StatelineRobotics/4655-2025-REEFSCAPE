5625eb22222b35183dc07bbad49740e7d9c49efa
Spaceman 11 <135277197+Spaceman113138@users.noreply.github.com>
2025-02-26 09:42:11 -0600

    Update WristIOSim.java

--- src/main/java/frc/robot/subsystems/Mechanisms/Wrist/WristIOSim.java
+++ src/main/java/frc/robot/subsystems/Mechanisms/Wrist/WristIOSim.java
@@ -1,1 +1,15 @@
 
+
+
+
+public class WristIOSim extends WristIOSparkMax {
+  
+  private SparkMaxSim motorSim;
+
+  
+  public WristIOSparkMax() {
+    super();
+    motorSim = new SparkMaxSim(m_wrist, DCMotor.getNEO(2));
+  }
+
+}
