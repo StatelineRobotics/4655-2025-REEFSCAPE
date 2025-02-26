42dda7a5b5fcb2f4d518e740b77b3938492c7f41
Spaceman 11 <135277197+Spaceman113138@users.noreply.github.com>
2025-02-26 11:36:18 -0600

    Update WristIOSim.java

--- src/main/java/frc/robot/subsystems/Mechanisms/Wrist/WristIOSim.java
+++ src/main/java/frc/robot/subsystems/Mechanisms/Wrist/WristIOSim.java
@@ -4,12 +4,35 @@
 
 public class WristIOSim extends WristIOSparkMax {
   
-  private SparkMaxSim motorSim;
+  private SparkFlexSim wristMotorSim;
+  private SparkAbsoluteEncoderSim wristEncoderSim;
+  private SparkMaxSim rightMotorSim;
+  private SparkReletiveEncoderSim rightEncoderSim;
+  private SparkMAxSim leftMotorSim;
+  private SparkReletiveEncoderSim leftEncoderSim;
 
   
   public WristIOSparkMax() {
     super();
-    motorSim = new SparkMaxSim(m_wrist, DCMotor.getNEO(2));
+    
+    wristMotorSim = new SparkFlexSim(m_wrist, DCMotor.getVotex(1));
+    wristEncoderSim = wristMotorSim.getAbsoluteEncoderSim();
+    SparkFlexConfig wConfig = new SparkFlexConfig();
+    wConfig.apply(getWristClosedLoopConfig)
+    m_wrist.configure(wConfig, noReset, noPersist);
+
+    leftMotorSim = new SparkMaxSim(m_leftIntake, DCMotor.getNeo550(1));
+    leftEncoderSim = leftMotorSim.getEncoderSim();
+
+    rightMotorSim = new SparkMaxSim(m_rightIntake, DCMotor.getNeo550(1));
+    rightEncoderSim = rightMotorSim.getEncoderSim();
+    
   }
 
+  private ClosedLoopConfig getWristClosedLoopConfig() {
+    ClosedLoopConfig config = new ClosedLoopConfig();
+    config.p(WristConstants.simKp)
+          .i(WristConstatnts.simKi)
+          .d(WristConstatns.simKd);
+    return config;
 }
