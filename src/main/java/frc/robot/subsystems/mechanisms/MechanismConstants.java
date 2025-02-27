b4f18c46d50f780da7806bba0552a2db11c24afa
Spaceman 11 <135277197+Spaceman113138@users.noreply.github.com>
2025-02-27 10:35:24 -0600

    fix mechConstatnts

--- src/main/java/frc/robot/subsystems/mechanisms/MechanismConstants.java
+++ src/main/java/frc/robot/subsystems/mechanisms/MechanismConstants.java
@@ -85,7 +85,7 @@ public class MechanismConstants {
   }
 
   public class RollerConstants {
-    public static final double currentLimit = `10;
+    public static final double currentLimit = 10;
 
     public static final double kp = 0.0;
     public static final double ki = 0.0;
