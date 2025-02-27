90de22f1edf6782382fd548c34324bcd03519695
= <ianspace3138@gmail.com>
2025-02-26 22:26:11 -0600

    remove special characters

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -190,7 +190,7 @@ public class RobotContainer {
             () -> -controller.getLeftX(),
             () -> -controller.getRightX()));
 
-    // Lock to 0° when A button is held
+    // Lock to 0 when A button is held
     controller
         .a()
         .whileTrue(
@@ -203,7 +203,7 @@ public class RobotContainer {
     // Switch to X pattern when X button is pressed
     controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
 
-    // Reset gyro to 0° when B button is pressed
+    // Reset gyro to 0 when B button is pressed
     controller
         .y()
         .onTrue(
